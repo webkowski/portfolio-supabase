ed12619176ca67b4fcba583d643c7445f919fffd
Piotr Łepkowski <ozrabal@gmail.com>
2023-09-07 13:57:41 +0200

    Update routing

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 const nextConfig = {
+  trailingSlash: true,
   experimental: {
     // serverActions: true,
   },
